0ddcc28b09bcb5afb1c4262a0fdbc28f4dc747fe
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2016-11-06 14:47:07 +1300

    created an accounts page

---
+++ mysite/code/Pages/AccountPage.php
@@ -0,0 +1,11 @@
+<?php
+class AccountPage extends WebAppPage {
+  private static $can_be_root = true;
+  private static $allowed_children = array ();
+
+
+}
+
+class AccountPage_Controller extends WebAppPage_Controller {
+
+}
