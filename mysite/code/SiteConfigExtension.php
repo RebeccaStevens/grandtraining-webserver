544c6eac3f4568921fe2a311311799fbf6e6a5dd
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2016-11-07 19:21:05 +1300

    fixed siteconfig extension

--- mysite/code/SiteConfigExtension.php
+++ mysite/code/SiteConfigExtension.php
@@ -1,19 +1,19 @@
 <?php
 class SiteConfigExtension extends DataExtension {
 
-  private static $db = array(
-    'facebookLink' => 'Varchar'
+  public static $db = array(
+    'FacebookLink' => 'Varchar'
   );
 
-  private static $has_one = array(
-    'logo' => 'Image'
+  public static $has_one = array(
+    'Logo' => 'Image'
   );
 
   public function updateCMSFields(FieldList $fields) {
-    $fields->addFieldsToTab('Root.Main', UploadField::create('logo', 'Logo'));
+    $fields->addFieldsToTab('Root.Main', UploadField::create('Logo', 'Logo'));
 
     $fields->addFieldsToTab('Root.Social', array(
-      TextField::create('facebookLink', 'Facebook')
+      TextField::create('FacebookLink', 'Facebook')
     ));
 
     $fields->removeFieldFromTab('Root.Main', 'Theme');
