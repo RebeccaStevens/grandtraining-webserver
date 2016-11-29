9a23e2637f375a71518825ef20c2f27a498bb69c
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2016-11-29 21:11:41 +1300

    moved database config into _ss_environment.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,15 +3,7 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	'type' => 'MySQLDatabase',
-	'server' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'SS_grandtraining',
-	'path' => ''
-);
-
 // Set the site locale
 i18n::set_locale('en_US');
+
+require_once('conf/ConfigureFromEnv.php');
