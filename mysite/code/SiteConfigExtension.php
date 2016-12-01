ca9eeb370a85fc58acd120325ad7fa16353f796c
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2016-12-01 18:59:01 +1300

    Added some email address settings to site config and formated the main tab

--- mysite/code/SiteConfigExtension.php
+++ mysite/code/SiteConfigExtension.php
@@ -2,7 +2,11 @@
 class SiteConfigExtension extends DataExtension {
 
   public static $db = array(
+    'BookingsEmail' => 'Varchar(256)',
+    'ContactEmail' => 'Varchar(256)',
+
     'FacebookLink' => 'Varchar(256)',
+
     'GoogleMapsZoom' => 'Int',
     'GoogleMapsApiKey' => 'Varchar',
     'RecaptchaApiSiteKey' => 'Varchar',
@@ -19,7 +23,15 @@ class SiteConfigExtension extends DataExtension {
     $fields->insertBefore(new Tab('Social', 'Social Media'), 'Access');
     $fields->insertBefore(new TabSet('3rdParty', '3rd Party'), 'Access');
 
-    $fields->addFieldsToTab('Root.Main', UploadField::create('Logo', 'Logo'));
+    $fields->addFieldsToTab('Root.Main', HeaderField::create(null, 'Company Settings'), 'Title');
+    $fields->addFieldsToTab('Root.Main', UploadField::create('Logo', 'Logo'), 'Title');
+    $fields->removeFieldFromTab('Root.Main', 'Tagline');
+
+    $fields->addFieldsToTab('Root.Main', HeaderField::create(null, 'Email Settings'));
+    $fields->addFieldsToTab('Root.Main', $field = EmailField::create('BookingsEmail', 'Bookings'));
+    $field->setDescription('When a user makes a bookings, a notification will be sent to this email address.');
+    $fields->addFieldsToTab('Root.Main', $field = EmailField::create('ContactEmail', 'Contact Form'));
+    $field->setDescription('Messages sent throught the contact us form will be sent to this address.');
 
     $fields->addFieldsToTab('Root.Social', array(
       TextField::create('FacebookLink', 'Facebook')
