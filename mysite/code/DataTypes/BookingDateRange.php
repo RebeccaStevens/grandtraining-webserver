8d7abf175480f31325d5c15668192df270798d5f
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2016-12-04 14:19:14 +1300

    added cms fields and data for booking date range

--- mysite/code/DataTypes/BookingDateRange.php
+++ mysite/code/DataTypes/BookingDateRange.php
@@ -15,4 +15,57 @@ class BookingDateRange extends DataObjectClient {
   private static $has_many = array(
     'Excludes' => 'Date'
   );
+
+  private static $summary_fields = array(
+    'HolidayClass.Title' => 'Holiday Class',
+    'Availability' => 'Availability',
+    'StartDate.Nice' => 'Start Date',
+    'EndDate.Nice' => 'End Date',
+    'Cost.Nice' => 'Cost'
+  );
+
+  private static $searchable_fields = array(
+    'Availability' => 'ExactMatchFilter',
+    'StartDate' => 'ExactMatchFilter',
+    'EndDate' => 'ExactMatchFilter',
+    'Cost' => 'ExactMatchFilter',
+    'HolidayClass.Title' => 'ExactMatchFilter'
+  );
+
+  public function getCMSFields() {
+    $fields = parent::getCMSFields();
+
+    $startDateField = DateField::create('StartDate', 'Start Date')
+      ->setConfig('dateformat', 'd/M/yyyy')
+      ->setConfig('min', date('c'));
+    $endDateField = DateField::create('EndDate', 'End Date')
+      ->setConfig('dateformat', 'd/M/yyyy')
+      ->setConfig('min', date('c'));
+
+    $startDateField->setDescription(sprintf('e.g. %s', Convert::raw2xml(Zend_Date::now()->toString($startDateField->getConfig('dateformat')))));
+    $endDateField->setDescription(sprintf('e.g. %s', Convert::raw2xml(Zend_Date::now()->toString($endDateField->getConfig('dateformat')))));
+		$startDateField->setAttribute('placeholder', $startDateField->getConfig('dateformat'));
+    $endDateField->setAttribute('placeholder', $endDateField->getConfig('dateformat'));
+
+    $fields->addFieldToTab('Root.Main', $startDateField);
+    $fields->addFieldToTab('Root.Main', $endDateField);
+
+    $fields->addFieldToTab('Root.Main', CurrencyField::create('Cost', 'Cost'));
+
+    $fields->addFieldToTab('Root.Main', DropdownField::create('Availability', 'Availability', singleton(__CLASS__)->dbObject('Availability')->enumValues()));
+
+    $fields->addFieldToTab('Root.Main', DropdownField::create('HolidayClassID', 'Holiday Class', HolidayClass::get()->map('ID', 'Title')->toArray()));
+
+    return $fields;
+  }
+
+  public function getCMSValidator() {
+    return new RequiredFields(array(
+      'StartDate',
+      'EndDate',
+      'Cost',
+      'Availability',
+      'HolidayClassID'
+    ));
+  }
 }
