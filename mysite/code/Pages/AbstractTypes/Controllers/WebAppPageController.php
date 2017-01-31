826ec9df61ad8220c7806ec0378345ca05dcab2f
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2017-01-31 20:54:09 +1300

    setup handling submission of the contact us form

--- mysite/code/Pages/AbstractTypes/Controllers/WebAppPageController.php
+++ mysite/code/Pages/AbstractTypes/Controllers/WebAppPageController.php
@@ -1,7 +1,32 @@
 <?php
 
 use SilverStripe\CMS\Controllers\ContentController;
+use SilverStripe\SiteConfig\SiteConfig;
 
 class WebAppPageController extends ContentController {
 
+  /**
+   * Checks if the Recaptcha was successful.
+   *
+   * @param {String} - The response from the client
+   * @return {Array} - The Recaptcha response
+   */
+  protected function checkRecaptcha($response) {
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+    $data = array(
+      'secret' => SiteConfig::current_site_config()->RecaptchaApiSecretKey,
+      'response' => $response,
+      'remoteip' => $_SERVER['REMOTE_ADDR']);
+
+    $options = array(
+      'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+      )
+    );
+
+    $context  = stream_context_create($options);
+    return json_decode(file_get_contents($url, false, $context), true);
+  }
 }
--- mysite/code/Pages/Types/Controllers/ContactUsPageController.php
+++ mysite/code/Pages/Types/Controllers/ContactUsPageController.php
@@ -1,5 +1,97 @@
 <?php
 
+use SilverStripe\Forms\Form;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\EmailField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\HiddenField;
+use SilverStripe\Forms\FormAction;
+use SilverStripe\Forms\RequiredFields;
+use SilverStripe\Control\Director;
+use SilverStripe\Control\HTTPRequest;
+
 class ContactUsPageController extends WebAppPageController {
 
+  private static $url_handlers = array(
+    'form-handler' => 'handleContactUs'
+  );
+
+  private static $allowed_actions = array (
+    'handleContactUs'
+  );
+
+  /**
+   * Handles the submission of the contact us form.
+   */
+  public function handleContactUs(HTTPRequest $request) {
+    if (!$request->isPost()) {
+      $this->httpError(404);
+      return;
+    }
+
+    if (Director::isDev()) {
+      header('Access-Control-Allow-Origin: *');
+    }
+
+    $form = $this->contactUsForm()->loadDataFrom($_POST);
+
+    if (!$form->validationResult()->isValid()) {
+      header('Content-Type: application/json');
+      echo json_encode(array('successful' => false));
+      return;
+    }
+
+    $data = $form->getData();
+
+    $recaptchaResponse = $this->checkRecaptcha($data['g-recaptcha-response']);
+
+    if ($recaptchaResponse['success'] === false) {
+      header('Content-Type: application/json');
+      echo json_encode(array('successful' => false));
+      return;
+    }
+
+    // save the message in the database
+    $message = new ContactMessage();
+    $message->Name = $data['name'];
+    $message->Phone = $data['phone'];
+    $message->Email = $data['email'];
+    $message->Location = $data['location'];
+    $message->Subject = $data['subject'];
+    $message->Message = $data['message'];
+    $message->Done = false;
+    $message->write();
+
+    header('Content-Type: application/json');
+    echo json_encode(array('successful' => true));
+    return;
+  }
+
+  /**
+   * Get the contact us form.
+   *
+   * @return Form
+   */
+  private function contactUsForm() {
+    $form = Form::create(
+      $this,
+      __FUNCTION__,
+      FieldList::create(
+        TextField::create('name'),
+        TextField::create('phone'),
+        EmailField::create('email'),
+        TextField::create('location'),
+        TextField::create('subject'),
+        TextareaField::create('message'),
+        HiddenField::create('g-recaptcha-response')
+      ),
+      FieldList::create(
+        FormAction::create('send')
+      ),
+      RequiredFields::create('name', 'phone', 'email', 'location', 'subject', 'message', 'g-recaptcha-response')
+    )->disableSecurityToken();
+
+    return $form;
+  }
 }
