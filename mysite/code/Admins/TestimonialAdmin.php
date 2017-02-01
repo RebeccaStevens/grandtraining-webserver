05563db0f403bd6344dafb3ab54aa2de6c94a5be
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2017-02-02 00:17:52 +1300

    added a Testimonial admin

---
+++ mysite/code/Admins/TestimonialAdmin.php
@@ -0,0 +1,16 @@
+<?php
+
+use SilverStripe\Admin\ModelAdmin;
+
+class TestimonialAdmin extends ModelAdmin {
+
+  private static $menu_title = 'Testimonials';
+
+  private static $url_segment = 'testimonials';
+
+  private static $managed_models = array(
+    'Testimonial'
+  );
+
+  // private static $menu_icon = '';
+}
