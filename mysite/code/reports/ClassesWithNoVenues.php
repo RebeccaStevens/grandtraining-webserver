e321c4d246a642c5661975e6471db3b93a87ccf8
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2017-02-02 02:26:56 +1300

    created a report to find classes not available at any venue

---
+++ mysite/code/reports/ClassesWithNoVenues.php
@@ -0,0 +1,34 @@
+<?php
+
+use SilverStripe\Reports\Report;
+use SilverStripe\ORM\ArrayList;
+use SilverStripe\ORM\Versioning\Versioned;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\DropdownField;
+
+class ClassesWithNoVenues extends Report {
+
+  public function title() {
+    return 'Classes not available at any venue';
+  }
+
+  public function sourceRecords($params = null) {
+    $result = ArrayList::create();
+
+    foreach (HolidayClass::get()->sort('Title') as $hc) {
+      if ($hc->AvailableVenues()->count() === 0) {
+        $result->push($hc);
+      }
+    }
+
+    return $result;
+  }
+
+  public function columns() {
+    $fields = array(
+      'Title' => 'Title'
+    );
+
+    return $fields;
+  }
+}
